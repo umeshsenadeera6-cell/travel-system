61ff1f46c0a2bab927fec07b6e24d65959785b97
Umesh Senadeera <umeshsenadeera6@gmail.com>
2026-04-02 09:00:30 +0530

    edit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import AntiGravityHero from '../components/AntiGravityHero';
+import Hero from '../components/Hero';
 import AboutUs from '../components/AboutUs';
 import ReviewCarousel from '../components/ReviewCarousel';
 
 export default function Home() {
   return (
     <main style={{ maxWidth: '1440px', margin: '0 auto', padding: '0 5%' }}>
-      <AntiGravityHero />
+      <Hero />
       <AboutUs />
       <ReviewCarousel />
     </main>
